608f757967aa30c4045df3ff8550db8228611a7d
mamazhakypova <mamazhakypovaa@bk.ru>
2024-02-26 00:50:46 +0600

    изменен

--- handlers/start.py
+++ handlers/start.py
@@ -1,12 +1,23 @@
 from aiogram import types, Dispatcher
 from config import bot
+from database import bot_db
+from keyboards import inline_buttons
 
 async def start_button(massage: types.Message):
     print(massage)
+    db = bot_db.Database()
+
+    db.sql_insert_user(
+        tg_id=massage.from_user.id,
+        username=massage.from_user.username,
+        first_name=massage.from_user.first_name,
+        last_name=massage.from_user.last_name
+        )
 
     await bot.send_message(
         chat_id=massage.from_user.id,
-        text=f'Hello {massage.from_user.first_name}'
+        text=f'Hello {massage.from_user.first_name}',
+        reply_markup=await inline_buttons.start_keyboard()
     )
 
 
--- main.py
+++ main.py
@@ -3,10 +3,16 @@ from config import dp
 from handlers import (
 start,
 )
+from database import bot_db
+
+async def on_startup(_):
+    db = bot_db.Database()
+    db.sql_create_tables()
 
 start.register_start_handlers(dp=dp)
 
 if __name__ == '__main__':
     executor.start_polling(
-       dp
+       dp,
+       on_startup=on_startup
     )
